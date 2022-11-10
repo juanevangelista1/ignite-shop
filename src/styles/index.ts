f7c2617f2c3a7d8a1e0a32134b5f23c3d31c1ef3
juanevangelista1 <juan.evangelista.nascimentoo@gmail.com>
2022-11-09 23:55:29 -0300

    Update pages

---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+﻿import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+} = createStitches({
+  theme: {
+    colors: {
+      rocketseat: '#8257e6',
+    }
+  }
+})
